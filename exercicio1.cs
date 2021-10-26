556d5101f3dda2a2815dc168f2f86d96c153ec92
Haline Dias <68698735+halineds@users.noreply.github.com>
2021-10-26 12:32:41 -0300

    Resolvendo exercício sobre medidas de um retângulo em c#

---
+++ exercicio1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+	class Program {
+		static void Main(string[] args) {
+
+			string produto 1 = "Computador";
+			string produto 2 = "Mesa de escritório";
+
+			byte idade = 30;
+			int codigo = 5290;
+			char genero = 'M';
+
+			double preco1 = 2100.0;
+			double preco2 = 650.50;
+			double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto 1} cujo preço é ${preco1:F2});
+            Console.WriteLine($"{produto 2}, cujo preco é  ${preco2:F2})");
+            
+			Console.WriteLine("Registro: $"{idade} anos de idade, código $"{codigo} e genero: $"{genero}");
+
+			Console.WriteLine("Medida com oito casas decimais:$"{medida:F8}");
+			
+
+
+			
+
+
+
+		}
+	}
+}
